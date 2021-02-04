ed425ba87badeaa3e3909608f41247bfbb9aca57
gifaniasofia <gifaniasofia@gmail.com>
2021-02-04 22:41:10 +0700

    login-register

--- server/controller/controller.js
+++ server/controller/controller.js
@@ -1,25 +1,66 @@
+const { User } = require("../models/")
+const { comparePassword } = require("../helpers/bcrypt")
+const { generateToken } = require("../helpers/jwt")
+
 class Controller {
-    static postLogin(req,res,next){
+  static postLogin(req,res,next){
+    const { email, password } = req.body
+    User.findOne({
+      where: { email }
+    })
+      .then((user) => {
+        if (!user) throw { status: 400, msg: "Invalid email or password" }
 
-    }
-    static postRegister(req,res,next){
+        const resultCompare = comparePassword(password, user.password)
 
-    }
-    static getJadwal(req,res,next){
+        if (!resultCompare) throw { status: 400, msg: "Invalid email or password" }
 
-    }
-    static getAlquran(req,res,next){
+        const access_token = generateToken({
+          id: user.id,
+          email: user.email,
+          city: user.city
+        })
 
-    }
-    static getKisah(req,res,next){
+        res.status(200).json({ access_token })
+      })
+      .catch((err) => {
+        console.log(err)
+        next(err)
+      })
 
-    }
-    static putSetting(req,res,next){
+  }
+  static postRegister(req,res,next){
+    const { name, email, password, city } = req.body
+    User.create({
+      name, email, password, city
+    })
+      .then((newUser) => {
+        res.status(201).json({
+          id: newUser.id,
+          email: newUser.email,
+          city: newUser.city
+        })
+      })
+      .catch((err) => {
+        next(err)
+      })
+  }
 
-    }
-    static postGoogle(req,res,next){
+  static getJadwal(req,res,next){
 
-    }
+  }
+  static getAlquran(req,res,next){
+
+  }
+  static getKisah(req,res,next){
+
+  }
+  static putSetting(req,res,next){
+
+  }
+  static postGoogle(req,res,next){
+
+  }
 }
 
 module.exports = Controller
