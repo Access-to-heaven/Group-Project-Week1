2d9b5d44ef2321712b60b3d4e932aa9e0b0f8136
muhammadsuryo31 <muhammadsuryo31@gmail.com>
2021-02-04 19:43:12 +0700

    config

---
+++ server/models/user.js
@@ -0,0 +1,38 @@
+'use strict';
+const {
+  Model
+} = require('sequelize');
+module.exports = (sequelize, DataTypes) => {
+  class User extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate(models) {
+      // define association here
+    }
+  };
+  User.init({
+    name:  DataTypes.STRING,
+    email: {
+      type:  DataTypes.STRING,
+      validate: {
+        isEmail: {msg: "not email format"}
+      },
+      unique: true
+    },
+    password: DataTypes.STRING,
+    city: DataTypes.STRING
+  }, {
+    sequelize,
+    modelName: 'User',
+    hooks: {beforeCreate: (user,option) => {
+      if (!user.city){
+        user.city = "Jakarta"
+      }
+      user.password = hash(user.password)
+    }}
+  });
+  return User;
+};
